eb53757eca4d4917a337460fadf1d5991685134b
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-01-08 11:07:19 +0000

    [DC-2186] Update reactivemongo dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "org.reactivemongo" %% "reactivemongo"      % "0.18.1",
+    "org.reactivemongo" %% "reactivemongo"      % "0.18.6",
     "uk.gov.hmrc"       %% "govuk-template"     % "5.3.0",
     "uk.gov.hmrc"       %% "play-ui"            % "7.40.0-play-25",
     "uk.gov.hmrc"       %% "bootstrap-play-25"  % "4.13.0",
